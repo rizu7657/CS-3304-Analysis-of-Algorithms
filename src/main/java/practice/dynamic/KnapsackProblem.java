4629e7c11641a3fc1d3d1b0aa3670179c031f27b
Ruhan <ruhanswanepoel@my.uopeople.edu>
2024-03-06 08:54:45 +0200

    U5 Knapsack Brute Force my impl

--- src/main/java/practice/dynamic/KnapsackProblem.java
+++ src/main/java/practice/dynamic/KnapsackProblem.java
@@ -3,30 +3,73 @@ package practice.dynamic;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.time.StopWatch;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class KnapsackProblem {
 
     public static void main(String[] args) {
         int capacity = 163;
-        int n = 10;
         Integer[] values = {10, 20, 15, 30, 25, 10, 20, 15, 30, 25};
         Integer[] weights = {4, 9, 15, 19, 27, 44, 54, 68, 73, 101};
 
         StopWatch watch = new StopWatch();
         watch.start();
-        solveByBruteForce(capacity, n, values, weights);
+        solveByBruteForce(capacity, values, weights, new ArrayList<>());
         watch.stop();
 
         System.out.printf("Brute force took %s microseconds.\n", watch.getTime(TimeUnit.MICROSECONDS));
 
         watch = new StopWatch();
         watch.start();
-        solveWithDynamicalP(capacity, n, values, weights);
+        solveWithDynamicalP(capacity, weights.length, values, weights);
         watch.stop();
 
         System.out.printf("Dynamic Programming took %s microseconds.", watch.getTime(TimeUnit.MICROSECONDS));
+    }
+
+    public static void solveByBruteForce(int K, Integer[] values, Integer[] weights, List<List<Item>> combinations) {
+        List<Item> options = new ArrayList<>();
+
+        for (int i = 0; i < weights.length; i++) {
+            options.add(new Item(i, values[i], weights[i]));
+        }
+
+        for (int i = 0; i < options.size(); i++) {
+
+            for (int j = 0; j < options.size(); j++) {
+                List<Item> combination = new ArrayList<>();
+
+                for (Item option : options) {
+
+                    if ((combination.stream().mapToInt(Item::getWeight).sum() + option.weight) > K) {
+                        continue;
+                    }
+                    combination.add(option);
+                }
+                combinations.add(combination);
+                Item temp = options.remove(0);
+                options.add(temp); // move front item to last position
+            }
+            Collections.shuffle(options);
+        }
+
+        List<Item> highestValueList = new ArrayList<>();
+        int highestValue = 0;
+
+        for (List<Item> comb : combinations) {
+            int sum = comb.stream().mapToInt(Item::getWeight).sum();
+
+            if (sum >= highestValue) {
+                highestValue = sum;
+                highestValueList = comb;
+            }
+            System.out.printf("\nSum of %s with weight %s and weights %s at %s", sum, comb.stream().mapToInt(Item::getWeight).sum(), ArrayUtils.toString(comb.stream().map(Item::getWeight).toList()), ArrayUtils.toString(comb.stream().map(Item::getPosition).toList()));
+        }
+
+        System.out.printf("\nBest combination is %s with value %s\n\n", ArrayUtils.toString(highestValueList.stream().map(Item::getWeight).toList()), highestValueList.stream().mapToInt(Item::getValue).sum());
     }
 
     public static void solveByBruteForce(int K, int n, Integer[] values, Integer[] weights) {
