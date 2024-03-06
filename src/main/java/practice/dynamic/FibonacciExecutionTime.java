ce9319daf5e4986c0af93b3323620dc4485869aa
Ruhan <ruhan.swanepoel17@gmail.com>
2024-03-06 21:38:32 +0200

    U5 Knapsack Brute force Improve running time

--- src/main/java/practice/dynamic/FibonacciExecutionTime.java
+++ src/main/java/practice/dynamic/FibonacciExecutionTime.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
-public class fibonacciExecutionTime {
+public class FibonacciExecutionTime {
 
     private static long[] Values;
 
--- src/main/java/practice/dynamic/KnapsackProblem.java
+++ src/main/java/practice/dynamic/KnapsackProblem.java
@@ -2,8 +2,10 @@ package practice.dynamic;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.time.StopWatch;
+import org.checkerframework.checker.units.qual.A;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -12,12 +14,12 @@ public class KnapsackProblem {
 
     public static void main(String[] args) {
         int capacity = 163;
-        Integer[] values = {10, 20, 15, 30, 25, 10, 20, 15, 30, 25};
+        Integer[] values = {4, 9, 15, 19, 27, 44, 54, 68, 73, 101};
         Integer[] weights = {4, 9, 15, 19, 27, 44, 54, 68, 73, 101};
 
         StopWatch watch = new StopWatch();
         watch.start();
-        solveByBruteForce(capacity, values, weights, new ArrayList<>());
+        solveByBruteForce(capacity, values, weights,false);
         watch.stop();
 
         System.out.printf("Brute force took %s microseconds.\n", watch.getTime(TimeUnit.MICROSECONDS));
@@ -30,63 +32,38 @@ public class KnapsackProblem {
         System.out.printf("Dynamic Programming took %s microseconds.", watch.getTime(TimeUnit.MICROSECONDS));
     }
 
-    public static void solveByBruteForce(int K, Integer[] values, Integer[] weights, List<List<Item>> combinations) {
+    public static void solveByBruteForce(int K, Integer[] values, Integer[] weights, boolean useGptMethod) {
+        List<List<Item>> combinations = new ArrayList<>();
         List<Item> options = new ArrayList<>();
 
         for (int i = 0; i < weights.length; i++) {
             options.add(new Item(i, values[i], weights[i]));
         }
 
-        for (int i = 0; i < options.size(); i++) {
+        if (useGptMethod){
+            combinations = generateCombinations(weights, values, K);
 
-            for (int j = 0; j < options.size(); j++) {
-                List<Item> combination = new ArrayList<>();
+        } else {
 
-                for (Item option : options) {
+            for (int i = 0; i < options.size(); i++) {
 
-                    if ((combination.stream().mapToInt(Item::getWeight).sum() + option.weight) > K) {
-                        continue;
+                for (int j = 0; j < options.size(); j++) {
+                    List<Item> combination = new ArrayList<>();
+
+                    for (Item option : options) {
+
+                        if ((combination.stream().mapToInt(Item::getWeight).sum() + option.weight) > K) {
+                            continue;
+                        }
+                        combination.add(option);
                     }
-                    combination.add(option);
+                    combinations.add(combination);
+                    Item temp = options.remove(0);
+                    options.add(temp); // move front item to last position
                 }
-                combinations.add(combination);
-                Item temp = options.remove(0);
-                options.add(temp); // move front item to last position
+                Collections.shuffle(options);
             }
-            Collections.shuffle(options);
         }
-
-        List<Item> highestValueList = new ArrayList<>();
-        int highestValue = 0;
-
-        for (List<Item> comb : combinations) {
-            int sum = comb.stream().mapToInt(Item::getWeight).sum();
-
-            if (sum >= highestValue) {
-                highestValue = sum;
-                highestValueList = comb;
-            }
-            System.out.printf("\nSum of %s with weight %s and weights %s at %s", sum, comb.stream().mapToInt(Item::getWeight).sum(), ArrayUtils.toString(comb.stream().map(Item::getWeight).toList()), ArrayUtils.toString(comb.stream().map(Item::getPosition).toList()));
-        }
-
-        System.out.printf("\nBest combination is %s with value %s\n\n", ArrayUtils.toString(highestValueList.stream().map(Item::getWeight).toList()), highestValueList.stream().mapToInt(Item::getValue).sum());
-    }
-
-    public static void solveByBruteForce(int K, int n, Integer[] values, Integer[] weights) {
-        List<List<Item>> combinations = new ArrayList<>();
-        List<Item> options = new ArrayList<>();
-
-        for (int k = 0; k < n; k++) {
-            options.add(new Item(k, values[k], weights[k]));
-        }
-
-        Integer[] positions = new Integer[n];
-
-        for (int i = 0; i < positions.length; i++) {
-            positions[i] = i;
-        }
-
-        combinations = generateCombinations(weights, values, K);
 
         List<Item> highestValueList = new ArrayList<>();
         int highestValue = 0;
@@ -98,10 +75,20 @@ public class KnapsackProblem {
                 highestValue = sum;
                 highestValueList = comb;
             }
-            System.out.printf("\nSum of %s with weight %s and weights %s at %s", sum, comb.stream().mapToInt(Item::getWeight).sum(), ArrayUtils.toString(comb.stream().map(Item::getWeight).toList()), ArrayUtils.toString(comb.stream().map(Item::getPosition).toList()));
+            System.out.printf("\nSum of %s with weight %s and weights %s at %s", sum,
+                    comb.stream().mapToInt(Item::getWeight).sum(),
+                    ArrayUtils.toString(comb.stream().map(Item::getWeight).toList()),
+                    ArrayUtils.toString(comb.stream().map(Item::getPosition).toList()));
+
+            if (highestValue == K) {
+                break;
+            }
         }
 
-        System.out.printf("\nBest combination is %s with value %s\n\n", ArrayUtils.toString(highestValueList.stream().map(Item::getWeight).toList()), highestValueList.stream().mapToInt(Item::getValue).sum());
+        System.out.printf("\nBest combination is %s with values %s with weights %s\n\n",
+                highestValueList.stream().mapToInt(Item::getValue).sum(),
+                ArrayUtils.toString(highestValueList.stream().map(Item::getValue).toList()),
+                highestValueList.stream().map(Item::getWeight).toList());
     }
 
     public static List<List<Item>> generateCombinations(Integer[] weights, Integer[] values, int limit) {
@@ -137,7 +124,37 @@ public class KnapsackProblem {
     }
 
     public static void solveWithDynamicalP(int K, int n, Integer[] values, Integer[] weights) {
+        // Populate base cases
+        int[][] mat = new int[n + 1][K + 1];
+        for (int r = 0; r < K + 1; r++) {
+            mat[0][r] = 0;
+        }
+        for (int c = 0; c < n + 1; c++) {
+            mat[c][0] = 0;
+        }
 
+        // Main logic
+        for (int item = 1; item <= n; item++) {
+            for (int capacity = 1; capacity <= K; capacity++) {
+                int maxValWithoutCurr = mat[item - 1][capacity]; // This is guaranteed to exist
+                int maxValWithCurr = 0; // We initialize this value to 0
+
+                int weightOfCurr = weights[item - 1]; // We use item -1 to account for the extra row at the top
+                if (capacity >= weightOfCurr) { // We check if the knapsack can fit the current item
+                    maxValWithCurr = values[item - 1]; // If so, maxValWithCurr is at least the value of the current item
+
+                    int remainingCapacity = capacity - weightOfCurr; // remainingCapacity must be at least 0
+                    maxValWithCurr += mat[item - 1][remainingCapacity]; // Add the maximum value obtainable with the remaining capacity
+                }
+
+                mat[item][capacity] = Math.max(maxValWithoutCurr, maxValWithCurr); // Pick the larger of the two
+            }
+        }
+
+        System.out.println(mat[n][K]); // Final answer
+        for (int[] ints : mat) {
+            System.out.println(Arrays.toString(ints));
+        }
     }
 
     private static class Item {
